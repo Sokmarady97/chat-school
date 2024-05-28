c1a6c9e7f17621b00717ec8d13e2b0e336453276
Sokmarady97 <sokmardy@pukyong.ac.kr>
2024-05-28 15:01:24 +0900

    chatbot.py update

--- dbconn/views/chatbot.py
+++ dbconn/views/chatbot.py
@@ -6,3 +6,4 @@ bp = Blueprint('chatbot', __name__, url_prefix='/edu_student')
 @bp.route('/')
 def show_chatbot_page():
     return render_template('./Student_page/Chatbot_Or_Communication_Page/Chatbot_or_communication_page.html')
+
